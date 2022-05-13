3d6714371d48daf0d40b76fa30c9b3073a738c95
Robert <robertjastrz@gmail.com>
2022-05-13 16:36:09 +0200

    day 7 python course

--- 100 Days of Code/main.py
+++ 100 Days of Code/main.py
@@ -338,36 +338,69 @@ import random
 # -------------------------------------password generator-------------------------------------------------------------
 
  # Password Generator Project
+#
+#
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+#            'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+#            'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+#
+# print("Welcome to the PyPassword Generator!")
+# nr_letters = int(input("How many letters would you like in your password?\n"))
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
+#
+# # Eazy Level - Order not randomised:
+# # e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+#
+# haslo = []
+# for letter in range(1, nr_letters +1):
+#     haslo+= random.choice(letters)
+#
+# for symbol in range(1,nr_symbols+1):
+#     haslo+=random.choice(symbols)
+#
+# for number in range(1,nr_numbers+1):
+#     haslo+=random.choice(numbers)
+# print(haslo)
+# random.shuffle(haslo)
+# print(haslo)
+#
+# hasło_nowe=""
+# for letter in haslo :
+#     hasło_nowe+=letter
+# print(hasło_nowe)
 
+# -----------------------------------------------function def-----------------------------------------------------------
+#
+# def moja_funkcja():
+#     print("hello!")
+#     print("bye")
+# moja_funkcja()
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-           'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-           'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+# -----------------------------------while ----------------------------------------------------------------------------
+# b=1
+# a=6
+# while b<a:
+#     print(b)
+#     b+=1
 
-print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+# --------------------------------Hangman  exercise---------------------------------------------------------------------
+import random
+#Step 1
 
-# Eazy Level - Order not randomised:
-# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+word_list = ["aardvark", "baboon", "camel"]
 
-haslo = []
-for letter in range(1, nr_letters +1):
-    haslo+= random.choice(letters)
-
-for symbol in range(1,nr_symbols+1):
-    haslo+=random.choice(symbols)
-
-for number in range(1,nr_numbers+1):
-    haslo+=random.choice(numbers)
-print(haslo)
-random.shuffle(haslo)
-print(haslo)
-
-hasło_nowe=""
-for letter in haslo :
-    hasło_nowe+=letter
-print(hasło_nowe)
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = random.choice(word_list)
+print(f"chosen_word = {chosen_word}")
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("guess a letter").lower()
+print(f"guess = {guess}")
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+for letter in chosen_word:
+    if letter==guess:
+        print(letter)
+    else:
+        print("guess")
