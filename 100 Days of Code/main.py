c79b413fb3336ce8e23378fe410f892031a36ebb
Robert <robertjastrz@gmail.com>
2022-05-15 20:08:43 +0200

    day 7 python course hangman finish

--- 100 Days of Code/main.py
+++ 100 Days of Code/main.py
@@ -392,14 +392,14 @@ import random
 import random
 from hangman_words import word_list
 from hangman_art import logo,stages
-#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
+import os
 
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
-#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+
 print(logo)
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -411,7 +411,7 @@ for _ in range(word_length):
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
-    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+
     if guess in display:
       print(f"{guess} has been already guessed")
     #Check guessed letter
@@ -423,7 +423,7 @@ while not end_of_game:
 
     #Check if user is wrong.
     if guess not in chosen_word:
-        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+
         print(f"You guessed {guess} and it is not in chosen word, You lose a life")
         lives-= 1
         if lives == 0:
@@ -438,5 +438,5 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
-    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
+
     print(stages[lives])
