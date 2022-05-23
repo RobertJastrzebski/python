dad7f8cf544cb4f2444907c12ec5c83344c05609
Robert <robertjastrz@gmail.com>
2022-05-23 19:30:03 +0200

    day 9 python

--- 100 Days of Code/main.py
+++ 100 Days of Code/main.py
@@ -510,6 +510,80 @@ import random
 #             end_text+=letter
 #     print(f"The encoded text is {end_text}")
 # cesar(plain_text=text,shift_amount=shift,cipher_direction=direction)
+# --------------------------------------------Dictionaries Nesting-----------------------------------------------------
 
+moje= {"lubie":1 ,"nie_lubie":2}
+
+# print(moje["lubie"])
+#
+# moje["nie_wiem"]="winogrona"
+#
+
+#
+# for key in moje:
+#     print(key)
+#     print(moje[key])
+# print(moje)
+# ----------------------------------dictionary exercise----------------------------------------------------------------
+# student_scores = {
+#     "Harry": 81,
+#     "Ron": 78,
+#     "Hermione": 99,
+#     "Draco": 74,
+#     "Neville": 62,
+# }
+# # 🚨 Don't change the code above 👆
+#
+# # TODO-1: Create an empty dictionary called student_grades.
+# student_grades = {}
+#
+# # TODO-2: Write your code below to add the grades to student_grades.👇
+# for student in student_scores:
+#     score = student_scores[student]
+#     if score > 90:
+#         student_grades[student] = "Outstanding"
+#     elif score > 80:
+#         student_grades[student] = "Exceeds Expectations"
+#     elif score > 70:
+#         student_grades[student] = "Acceptable"
+#     else:
+#         student_grades[student] = "Fail"
+#
+#     # 🚨 Don't change the code below 👇
+# print(student_grades)
+
+# --------------------------------------exercise dictionary------------------------------------------------------------
+
+# travel_log = [
+# {
+#   "country": "France",
+#   "visits": 12,
+#   "cities": ["Paris", "Lille", "Dijon"]
+# },
+# {
+#   "country": "Germany",
+#   "visits": 5,
+#   "cities": ["Berlin", "Hamburg", "Stuttgart"]
+# },
+# ]
+# #🚨 Do NOT change the code above
+#
+# #TODO: Write the function that will allow new countries
+# #to be added to the travel_log. 👇
+# def add_new_country(country_name,visits_number,cities_visited):
+#
+#   new_country={
+#     "country":country_name,
+#     "visits":visits_number,
+#     "cities":cities_visited
+#   }
+#   travel_log.append(new_country)
+#
+#
+#
+#
+# #🚨 Do not change the code below
+# add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+# print(travel_log)
 
 
