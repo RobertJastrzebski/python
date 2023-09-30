748185bc3d9475cfa050b3985eed6012cad9dba0
Robert <robertjastrz@gmail.com>
2023-09-30 13:56:29 +0200

    day_31_fiszki_project

--- day_26_nato_alphabet/main.py
+++ day_26_nato_alphabet/main.py
@@ -29,3 +29,5 @@ generate_phonetic()
 
 
 
+
+
---
+++ day_31_flashcard_project/main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+
+window.title("Fiszki")
+window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
+
