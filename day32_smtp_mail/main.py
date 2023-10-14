c9b560491719cb2220ea0808108a630dbd15d1c4
Robert <robertjastrz@gmail.com>
2023-10-14 11:59:18 +0200

    day_32_how_to_send_emails_using_smtp

---
+++ day32_smtp_mail/main.py
@@ -0,0 +1,14 @@
+import smtplib
+import ship
+
+
+my_email =ship.my_email
+reciver_email=ship.reciver_email
+password="pbrx xlke jabj vdcz"
+with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+    connection.starttls()
+    connection.login(user=my_email,password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs=reciver_email,
+        msg="Subject:Witam serdecznie \n\n Nowa wiadowmosc tekstowa ")
