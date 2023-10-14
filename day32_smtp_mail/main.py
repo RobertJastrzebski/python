9488b2f52163bd4463111f7da77572965abdecc9
Robert <robertjastrz@gmail.com>
2023-10-14 12:02:01 +0200

    day_32_how_to_send_emails_using_smtp small changes

--- day32_smtp_mail/main.py
+++ day32_smtp_mail/main.py
@@ -4,7 +4,7 @@ import ship
 
 my_email =ship.my_email
 reciver_email=ship.reciver_email
-password="pbrx xlke jabj vdcz"
+password=ship.password
 with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
     connection.starttls()
     connection.login(user=my_email,password=password)
